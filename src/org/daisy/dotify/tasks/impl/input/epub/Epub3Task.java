8b3b218dd948dc450d31b3109c8de812c404c3eb
Joel Håkansson <joel.hakansson@pm.me>
2019-04-11 13:54:19 +0200

    wip

--- src/org/daisy/dotify/tasks/impl/input/epub/Epub3Task.java
+++ src/org/daisy/dotify/tasks/impl/input/epub/Epub3Task.java
@@ -3,13 +3,22 @@ package org.daisy.dotify.tasks.impl.input.epub;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 import org.daisy.dotify.common.io.FileIO;
 import org.daisy.streamline.api.media.AnnotatedFile;
+import org.daisy.streamline.api.media.BaseFolder;
 import org.daisy.streamline.api.media.DefaultAnnotatedFile;
+import org.daisy.streamline.api.media.DefaultFileSet;
+import org.daisy.streamline.api.media.FileSet;
+import org.daisy.streamline.api.media.ModifiableFileSet;
 import org.daisy.streamline.api.option.UserOption;
 import org.daisy.streamline.api.tasks.InternalTaskException;
 import org.daisy.streamline.api.tasks.ReadWriteTask;
@@ -69,6 +78,74 @@ public class Epub3Task extends ReadWriteTask {
 		return new DefaultAnnotatedFile.Builder(output.toPath()).extension("html").mediaType("application/xhtml+xml").build();
 	}
 	
+	@Override
+	public ModifiableFileSet execute(FileSet input, BaseFolder output) throws InternalTaskException {
+		try {
+			Path unpacked = Files.createTempDirectory("epub");
+			try {
+				logger.info("Unpacking...");
+				ContentExtractor.unpack(Files.newInputStream(input.getManifest().getPath()), unpacked.toFile());
+				logger.info("Merging content files...");
+				ContainerReader container = new ContainerReader(unpacked.toFile());
+				ContentMerger merger = new ContentMerger(container);
+				if (opfPath==null && container.getOPFPaths().size()>1) {
+					StringBuilder sb = new StringBuilder("Epub container contains more than one OPF:");
+					for (String s : container.getOPFPaths()) {
+						sb.append(" ");
+						sb.append(s);
+					}
+					throw new InternalTaskException(sb.toString());
+				}
+				String _opfPath = opfPath==null?container.getOPFPaths().get(0):opfPath;
+				Path manifestFile = output.getPath().resolve("index.html");
+				merger.makeSingleContentDocument(_opfPath, manifestFile.toFile());
+				AnnotatedFile manifest = new DefaultAnnotatedFile.Builder(manifestFile).extension("html").mediaType("application/xhtml+xml").build();
+				DefaultFileSet.Builder builder = new DefaultFileSet.Builder(manifest);
+				moveResources(container, _opfPath, unpacked.resolve(_opfPath).getParent(), output.getPath())
+					.forEach(v->builder.add(v));
+				return builder.build();
+			} catch (EPUB3ReaderException e) {
+				throw new InternalTaskException(e);
+			} finally {
+				logger.info("Deleting temp folder: " + unpacked);
+				FileIO.deleteRecursive(unpacked.toFile());
+			}
+		} catch (IOException e) {
+			throw new InternalTaskException(e);
+		}
+	}
+	
+	private Stream<Path> moveResources(ContainerReader reader, String path, Path sourceFolder, Path targetFolder) throws EPUB3ReaderException {
+		OPF opf = reader.readOPF(path);
+		Map<String, String> manifest = new HashMap<>(opf.getManifest());
+		//remove spine items
+		for (String idref : opf.getSpine()) {
+			manifest.remove(idref);
+		}
+		return manifest.values().stream()
+			.filter(v->{
+					if (Files.exists(sourceFolder.resolve(v))) {
+						 return true;
+					} else {
+						 logger.info("Referenced file cannot be found: " + v);
+						 return false;
+					}
+				})
+			.map(v->{
+				Path source = sourceFolder.resolve(v);
+				Path target = targetFolder.resolve(v);
+				logger.fine("Moving " + source + " --> " + target);
+				target.toFile().getParentFile().mkdirs();
+				try {
+					Files.move(source, target, StandardCopyOption.REPLACE_EXISTING);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return target;
+				});
+	}
+
 	@Override
 	public List<UserOption> getOptions() {
 		List<UserOption> options = new ArrayList<>();
